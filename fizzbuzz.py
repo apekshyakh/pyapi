e6c385e25514b8475ed6c029f6604ceb271a6128
Apekshya Khanal <apekshyakh@gmail.com>
2021-01-13 15:57:57 +0000

    added instructions

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,7 +3,8 @@
 """ This is a commonly used challenge to see if applicants actually know how to code!
 Write a short program that prints each number from 1 to 100 on a new line
 For each multiple of 3, print "Fizz" instead of the number
-
+For each multiple of 5, print "Buzz" instead of the number. 
+For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the numbers
 """
 for x in range(1,101,):
     
